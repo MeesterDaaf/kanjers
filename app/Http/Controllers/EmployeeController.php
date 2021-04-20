ec0ba91608a104371f6aef010c214db371eef89e
Meesterdaaf <d.vanoudheusden@rocva.nl>
2021-04-21 00:58:24 +0200

    first feature

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'users' => User::all(),
+            'header' => 'Alle werknemers'
+        ];
+
+        return view('users.index', $data);
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @yield('main')
             </main>
         </div>
     </body>

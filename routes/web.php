d5ec944904f39b127629ad29ce5e2467846d6e85
Meesterdaaf <d.vanoudheusden@rocva.nl>
2021-04-23 06:50:20 +0200

    added middleware to all routes

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('users', [EmployeeController::class, 'index']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__ . '/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    // Route::get('/', function () {
+    //     return view('welcome');
+    // });
+
+    Route::get('users', [EmployeeController::class, 'index']);
+
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->middleware(['auth'])->name('dashboard');
+});

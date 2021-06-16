122a9a58d27a381aecfae9dd6731202e104e05cf
Meesterdaaf <d.vanoudheusden@rocva.nl>
2021-06-16 20:20:52 +0200

    permission seeder created

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -18,16 +20,31 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // create permissions
-        Permission::create(['name' => 'edit users']);
-        Permission::create(['name' => 'delete users']);
 
-        // this can be done as separate statements
-        $role = Role::create(['name' => 'employee']);
-        $role->givePermissionTo('edit users');
+        foreach (Area::get() as $item) {
+            Permission::create(['name' => 'index-' . $item]);
+            Permission::create(['name' => 'view-' . $item]);
+            Permission::create(['name' => 'edit-' . $item]);
+            Permission::create(['name' => 'create-' . $item]);
+            Permission::create(['name' => 'delete-' . $item]);
+        }
 
-
+        //create super admin user
         $role = Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Permission::all());
+        $user = User::where('title', 'Administrator')->first();
+        $user->assignRole('super-admin');
+
+        //create manager user
+        $role = Role::create(['name' => 'manager']);
+        $user = User::where('title', 'Manager')->first();
+        $user->assignRole('manager');
+        $role->givePermissionTo(['edit-users', 'index-roles']);
+
+        //create employee user
+        $role = Role::create(['name' => 'employee']);
+        $user = User::where('title', 'Employee')->first();
+        $user->assignRole('employee');
+        $role->givePermissionTo('index-users');
     }
 }
